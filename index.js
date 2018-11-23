9b422235462bc609bf3e9e3c5371a023b62e2a7d
Yana Ustsinovich <yustinovich@icloud.com>
2018-11-23 04:33:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 let currentUser = "Mania";
 let welcomeMessage = ("Welcome to Flatbook, " + currentUser + "!");
 let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = ("Welcome, " + currentUser[0] + "!");
